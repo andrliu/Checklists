0152b2404b97552e76fa637082600042c9e1183e
andrliu <dieseliu321@gmail.com>
2014-12-21 15:16:36 -0800

    add and delete func

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,20 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Andrew Liu on 12/21/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem
+{
+    var text = ""
+    var checked = false
+    
+    func toggleChecked()
+    {
+        checked = !checked
+    }
+}
